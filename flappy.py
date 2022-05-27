0c253fcae8a2dedf99f20bfae4a7dc0971c6679c
diggi55 <schaeffer-tobias@gmx.de>
2022-05-27 23:34:11 +0200

    Move variables to separate file

--- flappy.py
+++ flappy.py
@@ -1,23 +1,8 @@
 import pygame, random, time
 from pygame.locals import  *
+from vars import *
 
-#VARIABLES
-SCREEN_WIDHT = 400
-SCREEN_HEIGHT = 600
-SPEED = 20
-GRAVITY = 2.5
-GAME_SPEED = 15
 
-GROUND_WIDHT = 2 * SCREEN_WIDHT
-GROUND_HEIGHT  = 100
-
-PIPE_WIDHT = 80
-PIPE_HEIGHT = 500
-
-PIPE_GAP = 150
-
-wing = 'assets/audio/wing.wav'
-hit = 'assets/audio/hit.wav'
 
 pygame.mixer.init()
 
---
+++ vars.py
@@ -0,0 +1,22 @@
+
+#VARIABLES
+# TODO! use total hight/width ??
+SCREEN_WIDHT = 400
+
+SCREEN_HEIGHT = 600
+SPEED = 20
+GRAVITY = 2.5
+GAME_SPEED = 15
+
+GROUND_WIDHT = 2 * SCREEN_WIDHT
+GROUND_HEIGHT  = 100
+
+PIPE_WIDHT = 80
+PIPE_HEIGHT = 500
+
+# gap between top and bottom pipe
+# decrease over time??
+PIPE_GAP = 150
+
+wing = 'assets/audio/wing.wav'
+hit = 'assets/audio/hit.wav'
